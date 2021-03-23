0c9e107e639a9b9dfdd61d9d926a79a66b4e3c4a
estepona <esteponawondering@gmail.com>
2021-03-22 23:45:42 -0400

    Create 1-11.scm

---
+++ 1-11.scm
@@ -0,0 +1,21 @@
+#|
+ | A function f is defined by the rule that
+ | f(n) = n if n < 3,
+ |      = f(n-1) + 2f(n-2) + 3f(n-3) if n >= 3.
+ |#
+
+; write a procedure that computes f by means of a recursive process
+(define (fn n)
+  (if (< n 3) n
+      (+ (* 1 (fn (- n 1)))
+         (* 2 (fn (- n 2)))
+         (* 3 (fn (- n 3))))))
+
+; write a procedure that computes f by means of an iterative process
+(define (fn n)
+  (if (< n 3) n
+      (iter-fn 2 1 0 3 n)))
+
+(define (iter-fn a b c step n)
+  (if (> step n) a)
+      (iter-fn (+ a (* 2 b) (* 3 c)) a b (+ step 1) n))
