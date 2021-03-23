1f4cd4da7920a22749e23d5e094592fbd2defd04
estepona <esteponawondering@gmail.com>
2021-03-23 10:17:17 -0400

    Create 1-13.scm

---
+++ 1-13.scm
@@ -0,0 +1,7 @@
+#| 
+ | Prove that Fib(n) is the closest integer to a^n/(sqrt 5), where a = (1 + (sqrt 5))/2. Hint: Let b = (1 - (sqrt 5))/2.
+ | Use induction and the definition of the Fibonacci numbers to prove that Fib(n) = (a^n - b^n)/(sqrt 5).
+ |#
+
+; this article has the perfect solution that I don't have:
+;   https://codology.net/post/sicp-solution-exercise-1-13/
