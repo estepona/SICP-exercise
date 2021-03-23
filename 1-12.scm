c75ad22c57ec5e4f8ba4e3f08992ef224ca37e5c
Binghuan Zhang <esteponawondering@gmail.com>
2021-03-23 00:33:28 -0400

    Create 1-12.scm

---
+++ 1-12.scm
@@ -0,0 +1,8 @@
+#|
+ | Write a procedure that computes elements of Psacal's triangle by means of a recursive process.
+ |#
+
+(define (fn x y)
+  if (or (= x 1) (= y 1) (= x y)) 1
+     (+ (fn (- x 1) (- y 1))
+        (fn (- x 1) y))))
